7447442859cfe8cb53187166e98dc4c15f4c6848
Ajeet Kotturu <ajeet.kotturu@outlook.com>
2023-04-05 16:08:10 -0700

    Seed.ts file added and statement for inserting added to seed.ts. Records will be inserted soon.

--- prisma/migrations/20230308233808_init/migration.sql
+++ prisma/migrations/20230308233808_init/migration.sql
@@ -4,7 +4,13 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
     `firstName` VARCHAR(191) NULL,
     `lastName` VARCHAR(191) NULL,
+    `isManager` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Insert into tables
+
+
+

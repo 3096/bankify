c2ed2b4070e518f150035c4751acc383c6fec4f9
mayukh2002 <dasmayukh2002@gmail.com>
2023-04-11 19:08:00 -0700

    eh, kms
    if this map stops working, bury me

--- src/routes/(userhome)/searchATM/+page.svelte
+++ src/routes/(userhome)/searchATM/+page.svelte
@@ -1,50 +1,68 @@
 <script>
-    import NavBar from '$lib/components/NavBar.svelte';
+	import { onMount } from 'svelte';
+  
+	const API_KEY = 'AIzaSyCL_GqGwgwNVCN3WVwkZyOevvBDgX-KCZ0';
+  
+	onMount(() => {
+	  // Load the Google Maps API script
+	  const script = document.createElement('script');
+	  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+	  script.async = true;
+	  document.head.appendChild(script);
+  
+	  // Initialize the map when the script has loaded
+	  script.onload = () => {
+		const map = new google.maps.Map(document.getElementById('map'), {
+		  center: { lat: 37.7749, lng: -122.4194 }, // San Francisco
+		  zoom: 12
+		});
+	  };
+	});
   </script>
   
-  <!-- Welcome to p1
-  <h1>Menu Inbox Products Log out</h1>
+  <div id="map" style="height: 500px;"></div>
+
+
+
+
+
   
-  <h1>Accounts Dashboard</h1> -->
+  <!-- DO NOT TOUCH: This is for in case some crazy shit goes down -->
+
+
+  <!-- <script>
+    import NavBar from '$lib/components/NavBar.svelte';
+// Initialize and add the map
+    function initMap() {
+        // The location of Uluru
+        const casa = { lat: 37.317560, lng: -121.772160 };
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 10,
+            center: casa,
+        });
+        // The marker, positioned at Uluru
+        const marker = new google.maps.Marker({
+            position: casa,
+            map: map,
+        });
+    }
+
+    window.initMap = initMap;
+
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBIM43TX603u32yGJNP-hXJcHvYlgtETFs&callback=initMap';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+
+</script>
   
-  <!-- <div class="topnav">
-      <a class="active" href="#home">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-  </div> -->
-  
-  
+
  <div>
     <NavBar />
   </div>
 
-  <!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Display a map on a webpage</title>
-<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-<link href="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.css" rel="stylesheet">
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.js"></script>
-<style>
-body { margin: 0; padding: 0; }
-#map { position: absolute; top: 50; bottom: 50; width: 93%; height: 80%; }
-</style>
-</head>
-<body>
-<div id="map"></div>
-<script>
-	mapboxgl.accessToken = 'pk.eyJ1IjoibWlja2V5cHJpbWU3NDciLCJhIjoiY2xmanhpaTV5MDVlajNxbzZpNDJpYjcyNSJ9.O4yMrBklh-G21TnDwre5Ww';
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-    });
-</script>
-
-</body>
-</html>
-
+<div id="map"></div> -->
+  

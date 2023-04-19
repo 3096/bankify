d82ae905d572d404d1ba2594f25b52ec65ce0979
Ajeet Kotturu <ajeet.kotturu@outlook.com>
2023-04-19 16:04:40 -0700

    Add testing cases in test.ts. Modifications made in seed.ts but seed.ts is no longer being used for inserting data

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,21 @@ test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
+
+test('basic test', async ({ page }) => {
+  await page.goto('/signin');
+  await page.getByLabel('Email').fill('name');
+  await page.getByLabel('Password').fill('name');
+  await page.getByText('Sign in').click();
+  // ...
+});
+
+test('register test', async ({ page }) => {
+  await page.goto('/register');
+  await page.getByLabel('First Name').fill('name');
+  await page.getByLabel('Last Name').fill('name');
+  await page.getByLabel('Email').fill('name');
+  await page.getByLabel('Password').fill('name');
+  await page.getByText('Sign in').click();
+  // ...
+});

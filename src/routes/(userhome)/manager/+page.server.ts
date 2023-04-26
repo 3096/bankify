d986d2b453f7d269be1bf0bf8fd501689d379629
VolTorian <33360939+VolTorian@users.noreply.github.com>
2023-04-26 11:28:31 -0700

    manager page backend
    start to making manager backend query for accounts (skeleton for querying all accounts at the moment)

---
+++ src/routes/(userhome)/manager/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from './$types';
+import { validateSessionAndGetUserOrThrowRedirect } from '$lib/server/auth';
+import prisma from '$lib/server/prisma';
+
+export const load: PageServerLoad = async ({ locals }) => {
+  const userId = await validateSessionAndGetUserOrThrowRedirect(locals);
+  const user = await prisma.user.findMany({
+    include: {
+      accounts: {
+        orderBy: { createdAt: 'asc' }
+      }
+    }
+  });
+  return { user };
+};
--- src/routes/(userhome)/manager/+page.svelte
+++ src/routes/(userhome)/manager/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     //needs something to check isManager. only managers/bankify employees should be able to access this page
+  import { Button, Heading, P, Span } from 'flowbite-svelte';
+  import type { PageData } from './$types';
+  import { commaSeparateNumber } from '$lib/utils';
 
+  export let data: PageData;
 </script>
 
 <style>
@@ -47,7 +51,11 @@
   </div>
   <div class = "column h-full">Query Results
     <div class = "results">
-      test
+      {#each data.user as userAccount}
+        <tr>
+
+        </tr>
+      {/each}
     </div>
   </div>
 </div>

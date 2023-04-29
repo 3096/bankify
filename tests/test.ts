959f9bab47e697f8631bb61f20c7caa01536c668
Ajeet Kotturu <ajeet.kotturu@outlook.com>
2023-04-28 23:31:35 -0700

    add more test cases and update previous test cases

--- tests/test.ts
+++ tests/test.ts
@@ -17,6 +17,11 @@ export default defineConfig({
 
 test('basic test', async ({ page }) => {
   await page.goto('/');
+
+  await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/");
+  }).toPass();
+
   await page.click('input[name="email"]');
   await page.locator('input[name="email"]').fill('cheese@soup.com');
   await page.click('input[name="password"]');
@@ -25,9 +30,28 @@ test('basic test', async ({ page }) => {
   // ...
 });
 
-test('register test', async ({ page }) => {
+test('go to registration test', async ({ page }) => {
+  await page.goto('/');
 
+  await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/");
+  }).toPass();
+
+  await page.click('href[class="ml-1.5 text-blue-500 hover:text-primary-800"]');
+
+  /*await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/register");
+  }).toPass();*/
+  // ...
+});
+
+test('register test', async ({ page }) => {
   await page.goto('/register');
+
+  await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/register");
+  }).toPass();
+
   await page.click('input[name="firstName"]');
   await page.locator('input[name="firstName"]').fill('Spongebob');
   await page.click('input[name="lastName"]');
@@ -37,11 +61,21 @@ test('register test', async ({ page }) => {
   await page.click('input[name="password"]');
   await page.locator('input[name="password"]').fill('TheFormula');
   await page.click('button[class="btn btn-primary mt-6 transition duration-200 ease-in-out"]');
+
+  /*console.log(page.url());
+  await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/");
+  }).toPass();*/
   // ...
 });
 
 test('dashboard target test', async ({ page }) => {
   await page.goto('/dashboard');
+
+  await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/dashboard");
+  }).toPass();
+
   await page.locator('form[action="/targetdeal"]').locator('button[class="btn btn-info"]').click;
   // ...
   
@@ -65,17 +99,18 @@ test('dashboard nike test', async ({ page }) => {
   }).toPass(); */
 });
 
-/*test('account creation test', async ({ page }) => {
+test('account creation test', async ({ page }) => {
   await page.goto('/account-creation');
-  await page.getByLabel('First Name').fill('name');
-  await page.getByLabel('Last Name').fill('name');
-  await page.getByLabel('Email').fill('name');
-  await page.getByLabel('Password').fill('name');
-  await page.getByText('Sign in').click();
+
+  await expect(async () => {
+    expect(page.url()).toEqual("http://localhost:4173/account-creation");
+  }).toPass();
+
+  
   // ...
 });
 
-test('account number test', async ({ page }) => {
+/*test('account number test', async ({ page }) => {
   await page.goto('/account/[accountNumber]');
   await page.getByLabel('First Name').fill('name');
   await page.getByLabel('Last Name').fill('name');

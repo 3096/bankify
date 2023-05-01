5ca2b6890a82758e0daa0bcdb272deb8ddcada48
Ajeet Kotturu <ajeet.kotturu@outlook.com>
2023-05-01 03:56:55 -0700

    Have a total fo 8 automated browser tests. More tests coming soon

--- tests/test.ts
+++ tests/test.ts
@@ -38,7 +38,7 @@ test('incorrect login test', async ({}) => {
   // ...
 });
 
-//check this case again. Faisl to go to registration page
+
 test('go to registration test', async ({}) => {
   const browser = await chromium.launch({
     headless : false
@@ -48,7 +48,9 @@ test('go to registration test', async ({}) => {
 
   await page.goto('/');
 
-  page.click('href[class="ml-1.5 text-blue-500 hover:text-primary-800"]');
+  page.click('a[class="ml-1.5 text-blue-500 hover:text-primary-800"]');
+
+  //await page.close();
   // ...
 });
 
@@ -66,15 +68,88 @@ test('register test', async ({}) => {
   await page.click('input[name="lastName"]');
   await page.locator('input[name="lastName"]').fill('Squarepants');
   await page.click('input[name="email"]');
-  await page.locator('input[name="email"]').fill('spongeboy01@bikinibottom.com');
+  await page.locator('input[name="email"]').fill('spongeboy02@bikinibottom.com');
   await page.click('input[name="password"]');
   await page.locator('input[name="password"]').fill('TheFormula');
   await page.click('button[class="btn btn-primary mt-6 transition duration-200 ease-in-out"]');
+  
+  //await page.close();
+  // ...
+});
 
-  /*console.log(page.url());
-  await expect(async () => {
-    expect(page.url()).toEqual("http://localhost:4173/");
-  }).toPass();*/
+test('dashboard target test', async ({}) => {
+  const browser = await chromium.launch({
+    headless : false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage()
+
+  await page.goto('/');
+
+  await page.click('input[name="email"]');
+  await page.locator('input[name="email"]').fill('jim_carrey@themask.com');
+  await page.click('input[name="password"]');
+  await page.locator('input[name="password"]').fill('carrey123!');
+  await page.click('button[class="btn btn-primary mt-6 transition duration-200 ease-in-out"]');
+
+  await page.click('//html/body/div/div[1]/div/div[2]/main/div[2]/div[1]/form/button');
+  //await page.locator("/html/body/div/div[1]/div/div[2]/main/div[2]/div[1]/form/button").click;
+});
+
+test('dashboard starbucks test', async ({}) => {
+  const browser = await chromium.launch({
+    headless : false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage()
+
+  await page.goto('/');
+
+  await page.click('input[name="email"]');
+  await page.locator('input[name="email"]').fill('jim_carrey@themask.com');
+  await page.click('input[name="password"]');
+  await page.locator('input[name="password"]').fill('carrey123!');
+  await page.click('button[class="btn btn-primary mt-6 transition duration-200 ease-in-out"]');
+
+  await page.click('//html/body/div/div[1]/div/div[2]/main/div[2]/div[3]/form/button');
+});
+
+test('dashboard nike test', async ({}) => {
+  const browser = await chromium.launch({
+    headless : false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage()
+
+  await page.goto('/');
+
+  await page.click('input[name="email"]');
+  await page.locator('input[name="email"]').fill('jim_carrey@themask.com');
+  await page.click('input[name="password"]');
+  await page.locator('input[name="password"]').fill('carrey123!');
+  await page.click('button[class="btn btn-primary mt-6 transition duration-200 ease-in-out"]');
+
+  await page.click('//html/body/div/div[1]/div/div[2]/main/div[2]/div[5]/form/button');
+});
+
+test('customer help test', async ({}) => {
+  const browser = await chromium.launch({
+    headless : false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage()
+
+  await page.goto('/');
+
+  await page.click('input[name="email"]');
+  await page.locator('input[name="email"]').fill('jim_carrey@themask.com');
+  await page.click('input[name="password"]');
+  await page.locator('input[name="password"]').fill('carrey123!');
+  await page.click('button[class="btn btn-primary mt-6 transition duration-200 ease-in-out"]');
+
+  await page.click('//html/body/div/div[1]/div/div[2]/main/p[2]/a/button');
+
+  
   // ...
 });
 

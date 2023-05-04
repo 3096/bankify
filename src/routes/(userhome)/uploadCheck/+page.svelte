8a84c52c93e05123764b62eeb1f4451268a5af21
mayukh2002 <dasmayukh2002@gmail.com>
2023-05-03 17:43:49 -0700

    Conditionally Render UploadChequeConfirm Page

--- src/routes/(userhome)/uploadCheck/+page.svelte
+++ src/routes/(userhome)/uploadCheck/+page.svelte
@@ -9,7 +9,7 @@
   let image;
   let placeholder;
 	let showImage = false;
-  let file;
+  let file: Blob;
   let validType = false;
 
   function onChange() {
@@ -39,8 +39,27 @@
 		showImage = false; 
   }
 
+  let submit = false;
+  function togglesubmit(){
+    submit = !submit;
+  }
+
+  let imageSrc: string;
+  let showImage2 = false;
+  function retrieveImage() {
+    const base64Image = sessionStorage.getItem("uploadedCheck");
+    imageSrc = `data:image/png;base64,${base64Image}`;
+    showImage2 = true;
+    //console.log(base64Image);
+  }
+
+  let accountNo;
+  let amount;
+  //do stuff with an OCR to figure out accountNo and amount to deposit
+
 </script>
 
+{#if !submit}
 <form action = "/dashboard">
   <button class="btn btn-square btn-outline">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
@@ -54,10 +73,13 @@
   </div>
   <br>
   <input type="file" id="myFile" class="file-input file-input-bordered" name="filename" accept=".png, .jpeg, .jpg" bind:this={input} on:change={onChange}>
-  {#if file && validType}
+  <!-- {#if file && validType}
   <a href = "/uploadCheckConfirm">
     <input type="submit" class = "btn" accept=".png, .jpeg, .jpg" value = "Submit and Continue">
   </a>
+  {/if} -->
+  {#if file && validType}
+    <input on:click={() => togglesubmit()} type="submit" class = "btn" accept=".png, .jpeg, .jpg" value = "Submit and Continue">
   {/if}
 </form>
 <br>
@@ -68,3 +90,50 @@
 		<span bind:this={placeholder}>Image Preview</span>
 	{/if}
 </div>
+
+{:else}
+
+<script lang="ts">
+  let imageSrc;
+  let showImage2 = false;
+  function retrieveImage() {
+    const base64Image = sessionStorage.getItem("uploadedCheck");
+    imageSrc = `data:image/png;base64,${base64Image}`;
+    showImage2 = true;
+    //console.log(base64Image);
+  }
+
+  let accountNo;
+  let amount;
+  //do stuff with an OCR to figure out accountNo and amount to deposit
+
+</script>
+
+<form action = "/dashboard">
+  <button class="btn btn-square btn-outline">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+  </button>
+</form>
+<br>
+
+<form>
+  <input type = "text" placeholder = "Account Number" class = "input input-bordered">
+  <input type = "text" placeholder = "Routing Number" class = "input input-bordered">
+  <input type = "text" placeholder = "Check Number" class = "input input-bordered">
+  <input type = "text" placeholder = "Amount" class = "input input-bordered">
+  <br>
+  <input type = "submit" class = "btn">
+</form>
+<br>
+
+<form>
+  <button class="btn" on:click = {retrieveImage}>Show uploaded image test</button>
+</form>
+<div>
+	{#if showImage2}
+		<img  src={imageSrc} alt="Preview" />
+	{/if}
+</div>
+
+
+{/if}
